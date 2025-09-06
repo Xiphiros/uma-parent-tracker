03cb8b27dbb2ecb6351b600db35d4f1edd5a69db
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-06 15:42:26 -0400

    feat: create root app component and import styles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './css/styles.css';
+
+function App() {
+
+  return (
+    <div className="page">
+        {/* The rest of the application will be built here */}
+        <h1>React App Placeholder</h1>
+    </div>
+  )
+}
+
+export default App
