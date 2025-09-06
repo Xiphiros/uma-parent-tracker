3be8d74410dc82fa8d0f7e16bb2d3077db3c566b
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-06 15:49:21 -0400

    feat(ui): create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="header mb-8 text-center">
+            <h1 className="header__title text-4xl font-bold">Umamusume Parent Tracker</h1>
+            <p className="header__subtitle text-gray-600 mt-2">A point-based system for progressive parent farming.</p>
+            <div className="header__actions">
+                <label htmlFor="import-file" className="button button--secondary">Import Data</label>
+                <input type="file" id="import-file" className="visually-hidden" accept="application/json" />
+                <button id="export-btn" className="button button--neutral">Export Data</button>
+                <button id="theme-toggle-btn" className="theme-toggle" title="Toggle theme">
+                    <svg className="icon sun-icon" xmlns="http://www.w.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 2a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 2zM10 15a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 15zM10 7a3 3 0 100 6 3 3 0 000-6zM15.657 4.343a.75.75 0 10-1.06-1.06l-1.061 1.06a.75.75 0 001.06 1.06l1.06-1.06zM5.404 14.596a.75.75 0 10-1.06-1.06l-1.06 1.06a.75.75 0 001.06 1.06l1.06-1.06zM17.25 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 01.75.75zM4.25 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 01.75.75zM14.596 5.404a.75.75 0 001.06-1.06l-1.06-1.061a.75.75 0 10-1.06 1.06l1.06 1.06zM6.464 13.536a.75.75 0 001.06-1.06l-1.06-1.06a.75.75 0 10-1.06 1.06l1.06 1.06z" clipRule="evenodd" /></svg>
+                    <svg className="icon moon-icon" xmlns="http://www.w.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M7.455 2.104a.75.75 0 00-.965.572A9.003 9.003 0 002.5 12.162a9.003 9.003 0 008.59 8.592.75.75 0 00.572-.965A5.25 5.25 0 017.455 2.104z" /></svg>
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
