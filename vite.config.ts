46f9f3ea5ba5ce0452177f17f684a5f158d1e7f4
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-06 16:13:00 -0400

    feat(build): integrate tailwind css v4 using @tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/uma-parent-tracker/",
+  plugins: [react(), tailwindcss()],
+  base: "/Uma-Parent-Tracker/",
 })
