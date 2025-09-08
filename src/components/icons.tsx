3fdfaf9a172f51e5d6dffb8dc88202f84015e3a5
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-07 22:31:59 -0400

    fix(ui): remove margin from shared icon style to fix centering

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,6 @@ import { IconName } from "../types";
 const iconStyles: React.CSSProperties = {
     height: '1rem',
     width: '1rem',
-    marginRight: '0.25rem',
 };
 
 const DefaultIcon = () => <svg style={iconStyles} fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" /></svg>;
