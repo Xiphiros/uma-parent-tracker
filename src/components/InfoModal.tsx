a83eeaab445159f682a558743b7a9455d4385a8b
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-08 06:24:14 -0400

    chore(release): bump version to v0.6.0

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -6,7 +6,7 @@ interface InfoModalProps {
     onClose: () => void;
 }
 
-const APP_VERSION = '0.5.4';
+const APP_VERSION = '0.6.0';
 
 const InfoModal = ({ isOpen, onClose }: InfoModalProps) => {
     return (
