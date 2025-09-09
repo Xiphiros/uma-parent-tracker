596e1872fb0923f16de0e568344def100812b94c
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-09 00:03:43 -0400

    refactor(info-modal): implement UI localization

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,5 +1,6 @@
 import Modal from './common/Modal';
 import './InfoModal.css';
+import { useTranslation } from 'react-i18next';
 
 interface InfoModalProps {
     isOpen: boolean;
@@ -9,39 +10,38 @@ interface InfoModalProps {
 const APP_VERSION = '0.6.2';
 
 const InfoModal = ({ isOpen, onClose }: InfoModalProps) => {
+    const { t } = useTranslation(['info', 'common']);
     return (
-        <Modal isOpen={isOpen} onClose={onClose} title="About This Application" size="lg">
+        <Modal isOpen={isOpen} onClose={onClose} title={t('title')} size="lg">
             <div className="info-modal__content">
-                <h3 className="info-modal__app-title">Umamusume Parent Tracker</h3>
-                <p className="info-modal__meta">Version {APP_VERSION} (In Development) &bull; Created by Xiph</p>
+                <h3 className="info-modal__app-title">{t('appTitle')}</h3>
+                <p className="info-modal__meta">{t('meta', { version: APP_VERSION })}</p>
 
                 <div className="info-modal__section">
-                    <h4 className="info-modal__section-title">License</h4>
+                    <h4 className="info-modal__section-title">{t('licenseTitle')}</h4>
                     <p className="info-modal__text">
-                        This application is licensed under the MIT License.
+                        {t('licenseText')}
                     </p>
                     <p className="info-modal__text info-modal__text--muted mt-2">
-                        The underlying skill, character, and game data is sourced from the 'uma-tools' project, which is distributed under the GNU General Public License v3.0.
+                        {t('licenseMutedText')}
                     </p>
                 </div>
 
                 <div className="info-modal__section">
-                    <h4 className="info-modal__section-title">Data Source & Credits</h4>
+                    <h4 className="info-modal__section-title">{t('creditsTitle')}</h4>
                     <p className="info-modal__text">
-                        This tool would not be possible without the incredible work done by Alpha123 on the{' '}
-                        <a href="https://github.com/alpha123/uma-tools" target="_blank" rel="noopener noreferrer">uma-tools</a> project.
+                        {t('creditsText')}{' '}
+                        <a href="https://github.com/alpha123/uma-tools" target="_blank" rel="noopener noreferrer">{t('creditsLink')}</a> project.
                     </p>
                 </div>
                 
                 <div className="info-modal__section">
-                    <h4 className="info-modal__section-title">Special Thanks</h4>
-                    <p className="info-modal__text">
-                        Thank you to <em>Diabellstar</em> and <em>Sloovy</em> for their help with testing and feedback.
-                    </p>
+                    <h4 className="info-modal__section-title">{t('thanksTitle')}</h4>
+                    <p className="info-modal__text" dangerouslySetInnerHTML={{ __html: t('thanksText') }} />
                 </div>
 
                 <div className="dialog-modal__footer">
-                    <button className="button button--primary" onClick={onClose}>Close</button>
+                    <button className="button button--primary" onClick={onClose}>{t('common:close')}</button>
                 </div>
             </div>
         </Modal>
