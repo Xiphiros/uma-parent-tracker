bc1c587c44f57e29af912c074261c4421df9308f
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-09-24 21:30:40 -0400

    feat(calculator): update worker payload with conditional skills

--- src/utils/upgradeProbability.ts
+++ src/utils/upgradeProbability.ts
@@ -9,5 +9,6 @@ export type ProbabilityWorkerPayload = {
     skillMapEntries: [string, Skill][];
     spBudget: number;
     acquirableSkillIds: string[];
+    conditionalSkillIds: string[];
     targetAptitudes: string[];
 };
