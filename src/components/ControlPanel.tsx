e93435f64b27d192e50a923bd0d1de9c571c7051
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-11-09 13:13:49 -0400

    chore: remove unused react import

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './ControlPanel.css';
 import GoalDefinition from './GoalDefinition';
