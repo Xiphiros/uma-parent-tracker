62133eb96dc3a34f520a9c1125a776faceb3d4a6
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-11-09 14:23:36 -0400

    chore(release): bump version number to 0.12.0

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -7,7 +7,7 @@ interface InfoModalProps {
     onClose: () => void;
 }
 
-const APP_VERSION = '0.11.2';
+const APP_VERSION = '0.12.0';
 
 const InfoModal = ({ isOpen, onClose }: InfoModalProps) => {
     const { t } = useTranslation(['info', 'common']);
